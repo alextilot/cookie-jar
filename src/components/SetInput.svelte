cd1aac8fd281dbe40e40b8432af9ff7df78fa0dc
nezzely <alextilot@gmail.com>
2023-10-10 21:20:41 -0500

    feat: misc ui updates

---
+++ src/components/SetInput.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  interface Option {
+    id: number
+    text: string
+  }
+  let options: Option[] = [
+    {
+      id: 1,
+      text: `Cookie`,
+    },
+    {
+      id: 2,
+      text: `Local`,
+    },
+    {
+      id: 3,
+      text: `Session`,
+    },
+  ]
+
+  let storageType: Option | null
+
+  let storageKey = ''
+  let storageValue = ''
+
+  function handleSubmit() {
+    alert(`answered question ${storageType?.id} (${storageType?.text}) with "${storageKey}"`)
+  }
+</script>
+
+<h2>Storage</h2>
+<div class="flex w-full">
+  <form on:submit|preventDefault={handleSubmit}>
+    <select bind:value={storageType}>
+      {#each options as option}
+        <option value={option}>
+          {option.text}
+        </option>
+      {/each}
+    </select>
+
+    <input bind:value={storageKey} />
+    <input bind:value={storageValue} />
+    <button class="rounded-lg px-2 text-center font-semibold uppercase bg-blue-400" type="submit"
+      >Save</button
+    >
+  </form>
+</div>
--- src/popup/Popup.svelte
+++ src/popup/Popup.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import FilterButton from '@components/FilterButton.svelte'
+  import SetupInput from '@/components/SetInput.svelte'
   import '../tailwind.css'
 
   const crx = 'create-chrome-ext'
@@ -11,18 +11,10 @@
   chrome.storage.local.get(['key']).then((result) => {
     console.log('Value currently is ' + result.key)
   })
-  let name = 'world'
 </script>
 
-<main class="bg-slate-50">
-  <h3 class="p-4">Popup Page!</h3>
-
-  <h6>v 0.0.0</h6>
-  <ul>
-    <FilterButton />
-  </ul>
-
-  <a href="https://www.npmjs.com/package/create-chrome-ext" target="_blank">Power by {crx}</a>
+<main class="bg-slate-400">
+  <SetupInput />
 </main>
 
 <style>
@@ -32,40 +24,12 @@
   }
 
   :global(body) {
-    min-width: 20rem;
+    min-width: 40rem;
   }
 
   main {
     text-align: center;
     padding: 1em;
     margin: 0 auto;
-  }
-
-  h3 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 1.5rem;
-    font-weight: 200;
-    line-height: 1.2rem;
-    margin: 2rem auto;
-  }
-
-  h6 {
-    font-size: 0.5rem;
-    color: #333333;
-    margin: 0.5rem;
-  }
-
-  a {
-    font-size: 0.5rem;
-    margin: 0.5rem;
-    color: #cccccc;
-    text-decoration: none;
-  }
-
-  @media (min-width: 480px) {
-    h3 {
-      max-width: none;
-    }
   }
 </style>
